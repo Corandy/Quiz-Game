d50e535774998c69afa22d1e4819d6a3e82f5b30
Corandy <tijntje_226_91@hotmail.com>
2019-11-08 12:53:10 +0100

    Update sessionUser.js

--- src/actions/sessionUser.js
+++ src/actions/sessionUser.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 //will send mail adress of user to the server to verify if its unique
 //if succesfull it will fill in the user session data, with this the user is loggedin
 export const loginByMailAddress = (email = false) => {  
+  let root = 'http://' + document.location.hostname === 'localhost' ? document.location.hostname+':'+window.location.port : document.location.hostname;
   return function(dispatch) {
-    return axios.post(document.location.hostname+`/api/verify`, {email: email})
+    return axios.post(root+`/api/verify`, {email: email})
     .then(res => {
       if(res.data === 'Already Used') {
         throw ('Already used');
