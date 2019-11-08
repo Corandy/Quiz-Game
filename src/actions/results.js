259f8030a14a978b9bbfe5be5fca4dc44e517965
Corandy <tijntje_226_91@hotmail.com>
2019-11-08 13:53:25 +0100

    Update results.js

--- src/actions/results.js
+++ src/actions/results.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 //get result from node server based on userId. userId is an encrypted mail string
 //when succesful it will update the redux results state
 export const getUserResults = (userId) => {  
+    let root = document.location.hostname === 'localhost' ? 'http://' + document.location.hostname+':'+window.location.port : 'https://' + document.location.hostname;
     return function(dispatch) {
-      return axios.get(`http://localhost:3000/api/results/`+userId)
+      return axios.get(root+'/api/results/'+userId)
       .then(res => {
         dispatch({
           type: 'SET_USER_RESULTS',
@@ -20,8 +21,9 @@ export const getUserResults = (userId) => {
   //get results from all players from  node server
   //when succesful it will update the redux results state
   export const getAllResults = () => {  
+    let root = document.location.hostname === 'localhost' ? 'http://' + document.location.hostname+':'+window.location.port : 'https://' + document.location.hostname;
     return function(dispatch) {
-      return axios.get(`http://localhost:3000/api/results`)
+      return axios.get(root+'/api/results')
       .then(res => {
         dispatch({
           type: 'SET_ALL_RESULTS',
@@ -37,6 +39,7 @@ export const getUserResults = (userId) => {
   //send anwsers to the node server
   //node server will validate the answers and add a score to it and saves it on the server
   export const sendResults = (userId = false, results = []) => {
+    let root = document.location.hostname === 'localhost' ? 'http://' + document.location.hostname+':'+window.location.port : 'https://' + document.location.hostname;
     let answers = [];
     for(var i=0; i < results.length; i++) {
       let answer = {};
@@ -45,7 +48,7 @@ export const getUserResults = (userId) => {
       answers.push(answer);
     }
     return function(dispatch) {
-      return axios.post(`http://localhost:3000/api/results`, {userId: userId, answers:answers})
+      return axios.post(root+'api/results', {userId: userId, answers:answers})
       .then(res => {
         return true;     
       }).catch(err => {
